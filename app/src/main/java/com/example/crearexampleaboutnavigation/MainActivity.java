51ec778ba5a71a28a8dcb070f193ed6471389380
Fer0605 <2002003@utrivieramaya.edu.mx>
2022-02-14 15:09:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/crearexampleaboutnavigation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.crearexampleaboutnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void After(View view){
+        Intent After = new Intent(this, MainActivity2.class);
+        startActivity(After);
+    }
+}
